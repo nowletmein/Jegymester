3168f8303cc558c873bc610bbf139a9b61eb5e76
Ben <gerlingerbenn@gmail.com>
2026-04-21 14:58:09 +0200

    User Controller and User Service + Interface init

--- Backend/JegymesterApp.Services/UserService.cs
+++ Backend/JegymesterApp.Services/UserService.cs
@@ -5,12 +5,39 @@ using System.Text;
 
 namespace JegymesterApp.Services
 {
-    public class UserService
+    public interface IUserService
     {
-        public interface IUserService {
-            Task<int> Create(TicketCreateDto ticketCreateDto);
-            Task<TicketDto> Get(int ticketId);
-            Task<int> Delete(int ticketId);
+        Task<int> Create(TicketCreateDto ticketCreateDto);
+        Task<TicketDto> Get(int ticketId);
+        Task<int> Delete(int ticketId);
+        Task<int> Edit();
+        Task<int> Login(string email, string password);
+    }
+    public class UserService : IUserService
+    {
+        public Task<int> Create(TicketCreateDto ticketCreateDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(int ticketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Edit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TicketDto> Get(int ticketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Login(string email, string password)
+        {
+            throw new NotImplementedException();
         }
     }
 }
