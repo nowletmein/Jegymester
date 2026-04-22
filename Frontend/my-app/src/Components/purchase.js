a10fc96204936418af5a77b9c22a67862ad7f898
Ben <gerlingerbenn@gmail.com>
2026-04-22 19:37:35 +0200

    Fix: fixed purchase component

--- Frontend/my-app/src/Components/purchase.js
+++ Frontend/my-app/src/Components/purchase.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useNavigate } from 'react';
 import { useLocation } from 'react-router-dom';
 import '../Components/style/comp.css';
 import Header from './header.js';
